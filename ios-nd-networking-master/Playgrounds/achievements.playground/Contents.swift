f83f7d7184a9a660026cb296ffad9fce816972f2
Seab <honeyboyseab@gmail.com>
2016-07-14 23:55:26 -0400

    parsed the achievementsJSON object

--- ios-nd-networking-master/Playgrounds/achievements.playground/Contents.swift
+++ ios-nd-networking-master/Playgrounds/achievements.playground/Contents.swift
@@ -22,6 +22,62 @@ var parsedAchievementsJSON = try! NSJSONSerialization.JSONObjectWithData(rawAchi
 
 func parseJSONAsDictionary(dictionary: NSDictionary) {
     /* Start playing with JSON here... */
+    // get the top level dictionaries for achievements and categories
+    guard let achievementDictionaries = parsedAchievementsJSON["achievements"] as? [NSDictionary] else {
+        print("Cannot find key 'achievements' in \(parsedAchievementsJSON)")
+        return
+    }
+    
+    guard let categoryDictionaries = parsedAchievementsJSON["categories"] as? [NSDictionary] else {
+        print("Cannot find 'categories' in \(parsedAchievementsJSON)")
+        return
+    }
+    
+    var pointsTotal = 0
+    
+    // create an array to hold the categoryIds for "matchmaking" categories
+    var matchMakingIds: [Int] = []
+    
+    // create dictionary to store the counts for "matchmaking" categories
+    var categoryCounts: [Int: Int] = [:]
+    
+    // store all matchmaking categories
+    for categoryDictionary in categoryDictionaries {
+        
+        if let title = categoryDictionary["title"] as? String where title == "Matchmaking" {
+            guard let children = categoryDictionary["children"] as? [NSDictionary] else {
+                print("Cannot find key 'children' in \(categoryDictionary)")
+                return
+            }
+            
+            for child in children {
+                guard let categoryID = child["categoryID"] as? Int else {
+                    print("Cannot find key 'categoryID' in \(child)")
+                    return
+                }
+                
+                matchMakingIds.append(categoryID)
+            }
+        }
+    }
+    
+    for achievementDictionary in achievementDictionaries {
+        // add to point total and print if achievement has greater than 10 points
+        guard let points = achievementDictionary["points"] as? Int else {
+            print("Cannot find key 'points' in \(achievementDictionary)")
+            return
+        }
+        
+        pointsTotal += points
+        
+        // how many achievements have a point value greater than 10?
+        if points > 10 {
+            print("achievement has a point value greater than 10")
+        }
+    }
+    
+    
+    
 }
 
 parseJSONAsDictionary(parsedAchievementsJSON)
